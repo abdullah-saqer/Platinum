ebd165496c74367f437bc62ddcfd878212f81f05
Montaser Qasem <montaser.tutu@gmail.com>
2016-09-13 23:45:12 +0300

    Adding overflow rule on the cms index page

--- test.php
+++ test.php
@@ -1,28 +1,19 @@
-<head>
-<link rel="stylesheet" type="text/css" href="ckeditor/samples/css/samples.css">
-<link rel="stylesheet" type="text/css" href="ckeditor/samples/toolbarconfigurator/lib/codemirror/neo.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="ckeditor/ckeditor.js"></script>
-<script type="text/javascript" src="ckeditor/samples/js/sample.js"></script>
-</head>
-<script type="text/javascript">
-	$(document).ready(function(){
-		initSample();
+<?php
 
-		$("#submit").click(function(){
-			var s = CKEDITOR.instances['editor'].getData();
-			CKEDITOR.instances['editor'].setData("<h1>Montaser Qasem</h1>");
-		});
-
-	});
+$url = 'http://appigniters.com/testParam.php?test=montaser';
 
 
-</script>
-<body>
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'GET'
+    )
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
 
-	<div id="editor">
-		
-	</div>
-	<br>
-		<button id="submit">Submit</button>
-</body>
+
+var_dump($result);
+
+
+?>
