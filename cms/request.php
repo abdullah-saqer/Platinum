ba5dd2cd77153d1616a0638b0ed99b98937675a2
Montaser Qasem <montaser.tutu@gmail.com>
2016-09-20 19:41:28 +0300

    Getting the update from nadeem

--- cms/request.php
+++ cms/request.php
@@ -18,9 +18,9 @@ checklogin();
 
 			<div id="Content_Box">
 				 
-				 <center><div id="top_bar"></div></center>
+				<center><div id="top_bar"></div></center>
 
-				 <section id="Customer_Request_Box_Background">
+				 <div id="Customer_Request_Box_Background">
 				 	
 				 	<section id="Customer_Request_inner_Box">
 				 		
@@ -45,7 +45,7 @@ checklogin();
 				 	
 				 	</section>
 
-				 </section>
+				 </div>
 
 				 <section id="Customer_Request_Box_Background">
 				 	
@@ -73,158 +73,10 @@ checklogin();
 				 	</section>
 
 				 </section>
-
-
-			</div>
+            </div>
 		
 		</div>
 
 		<!--  -->
-		<div id="item_request_box">
-			<table id="table_box">
-				<tr> 
-                    <td style="border-left:none;">Customer Name</td>
-                    <td>Mobile</td>
-                    <td>Location</td>
-				</tr>
-			</table>
-
-			<div id="Content_Box">
-				 
-				 <center><div id="top_bar"></div></center>
-
-				 <section id="Customer_Request_Box_Background">
-				 	
-				 	<section id="Customer_Request_inner_Box">
-				 		
-				 		<img src="../items_photos/2.jpg" />
-				 		
-				 		<div id="img_bottom_text">
-				 		  	
-				 		  	<table id="img_bottom_table">
-				 		  	   <tr>
-				 		  	     <td id="item_name">Item Name </td>
-				 		  	     <td id="Quantity">3</td>
-				 		  	   </tr>
-				 		  	</table>
-				 		</div>
-
-				 		<span id="request_date">Request Date (20/12/2016)</span>
-
-				 		
-				 			<i class="fa fa-check" id="confirm" onclick="alert('confirm')"></i>
-				 			<i class="fa fa-close" id="refuse" onclick="alert('refuse')"></i>
-				 	    
-				 	
-				 	</section>
-
-				 </section>
-
-				 <section id="Customer_Request_Box_Background">
-				 	
-				 	<section id="Customer_Request_inner_Box">
-				 		
-				 		<img src="../items_photos/1.jpg" />
-				 		
-				 		<div id="img_bottom_text">
-				 		  	
-				 		  	<table id="img_bottom_table">
-				 		  	   <tr>
-				 		  	     <td id="item_name">Item Name </td>
-				 		  	     <td id="Quantity">3</td>
-				 		  	   </tr>
-				 		  	</table>
-				 		</div>
-
-				 		<span id="request_date">Request Date (20/12/2016)</span>
-
-				 		
-				 			<i class="fa fa-check" id="confirm" onclick="alert('confirm')"></i>
-				 			<i class="fa fa-close" id="refuse" onclick="alert('refuse')"></i>
-				 	    
-				 	
-				 	</section>
-
-				 </section>
-
-
-			</div>
-		
-		</div>
-
-<!--  -->
-
-
-		<div id="item_request_box">
-			<table id="table_box">
-				<tr> 
-                    <td style="border-left:none;">Customer Name</td>
-                    <td>Mobile</td>
-                    <td>Location</td>
-				</tr>
-			</table>
-
-			<div id="Content_Box">
-				 
-				 <center><div id="top_bar"></div></center>
-
-				 <section id="Customer_Request_Box_Background">
-				 	
-				 	<section id="Customer_Request_inner_Box">
-				 		
-				 		<img src="../items_photos/2.jpg" />
-				 		
-				 		<div id="img_bottom_text">
-				 		  	
-				 		  	<table id="img_bottom_table">
-				 		  	   <tr>
-				 		  	     <td id="item_name">Item Name </td>
-				 		  	     <td id="Quantity">3</td>
-				 		  	   </tr>
-				 		  	</table>
-				 		</div>
-
-				 		<span id="request_date">Request Date (20/12/2016)</span>
-
-				 		
-				 			<i class="fa fa-check" id="confirm" onclick="alert('confirm')"></i>
-				 			<i class="fa fa-close" id="refuse" onclick="alert('refuse')"></i>
-				 	    
-				 	
-				 	</section>
-
-				 </section>
-
-				 <section id="Customer_Request_Box_Background">
-				 	
-				 	<section id="Customer_Request_inner_Box">
-				 		
-				 		<img src="../items_photos/1.jpg" />
-				 		
-				 		<div id="img_bottom_text">
-				 		  	
-				 		  	<table id="img_bottom_table">
-				 		  	   <tr>
-				 		  	     <td id="item_name">Item Name </td>
-				 		  	     <td id="Quantity">3</td>
-				 		  	   </tr>
-				 		  	</table>
-				 		</div>
-
-				 		<span id="request_date">Request Date (20/12/2016)</span>
-
-				 		
-				 			<i class="fa fa-check" id="confirm" onclick="alert('confirm')"></i>
-				 			<i class="fa fa-close" id="refuse" onclick="alert('refuse')"></i>
-				 	    
-				 	
-				 	</section>
-
-				 </section>
-
-
-			</div>
-		
-		</div>
-
+		<!--  -->
 
